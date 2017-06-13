17e65e0d2791fcbbf6be33a8883d7d188b70572b
Mykola Dzioba <dzioba99@gmail.com>
2017-06-13 23:48:47 +0300

    Fixed server, AppInit

--- src/main/java/iful/edu/practice/init/AppInit.java
+++ src/main/java/iful/edu/practice/init/AppInit.java
@@ -1,5 +1,8 @@
 package iful.edu.practice.init;
 
+import javax.servlet.Filter;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
@@ -12,12 +15,20 @@ public class AppInit extends AbstractAnnotationConfigDispatcherServletInitialize
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
 
-		return new Class<?>[] { WebAppConfig.class };
+		return null;
 	}
 
 	@Override
 	protected String[] getServletMappings() {
 		return new String[] { "/" };
+	}
+
+	@Override
+	protected Filter[] getServletFilters() {
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		characterEncodingFilter.setForceEncoding(true);
+		return new Filter[] { characterEncodingFilter };
 	}
 
 }
